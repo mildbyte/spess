cbbf3d75a523d5bd10d5fdcf0ae1061c356569f2
Artjoms Iskovs <ai280@cam.ac.uk>
2014-04-08 19:16:41 +0300

    Added some basic classes

---
+++ spess/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spess
+{
+    class Owner
+    {
+        string name;
+        List<Station> ownedStations;
+        List<Ship> ownedShips;
+        int accountBalance;
+    }
+}
---
+++ spess/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spess
+{
+    class Ship
+    {
+        Dictionary<Good, int> cargo;
+        Sector currentSector;
+    }
+}
