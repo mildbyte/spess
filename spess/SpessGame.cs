e1b80c5954afd69692d9a70199e9911fea9bc611
Artjoms Iskovs <ai280@cam.ac.uk>
2014-04-11 18:41:19 +0300

    Rendering icons as quads

--- spess/SpessGame.cs
+++ spess/SpessGame.cs
@@ -29,6 +29,7 @@ namespace spess
         MouseState originalMouseState;
         Matrix viewMatrix;
         Matrix projectionMatrix;
+        Texture2D shipTex;
 
         private void UpdateViewMatrix()
         {
@@ -112,15 +113,12 @@ namespace spess
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             font = Content.Load<SpriteFont>("Arial");
+            shipTex = Content.Load<Texture2D>("ship");
 
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 0.3f, 1000.0f);
             UpdateViewMatrix();
 
-            basicEffect = new BasicEffect(GraphicsDevice)
-            {
-                TextureEnabled = true,
-                VertexColorEnabled = true,
-            };
+            basicEffect = new BasicEffect(GraphicsDevice);
 
             Mouse.SetPosition(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
             originalMouseState = Mouse.GetState();
@@ -160,40 +158,110 @@ namespace spess
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            //Owner testOwner = new Owner();
-            //Sector testSector = new Sector();
-            //Ship testShip = new Ship("SHIP!!1", new Location(testSector, new Vector3(0, 0, 0)), testOwner, 10.0);
-            //testSector.AddShip(testShip);
+            Owner testOwner = new Owner();
+            Sector testSector = new Sector();
+            Ship testShip = new Ship("SHIP!!1", new Location(testSector, new Vector3(0, 0, 0)), testOwner, 10.0);
+            testSector.AddShip(testShip);
 
-            //VertexPositionTexture[] vertices = new VertexPositionTexture[6];
-            //int i = 0;
+            Vector3 center = testShip.Location.Coordinates;
 
-            //Vector3 center = testShip.Location.Coordinates;
+            VertexPositionTexture[] vertices = new VertexPositionTexture[]
+                        {
+                            new VertexPositionTexture(
+                                new Vector3(0,1,0),
+                                new Vector2(0,1)),
+                            new VertexPositionTexture(
+                                new Vector3(0,0,0),
+                                new Vector2(0,0)),
+                            new VertexPositionTexture(
+                                new Vector3(1,1,0),
+                                new Vector2(1,1)),
+                            new VertexPositionTexture(
+                                new Vector3(1,0,0),
+                                new Vector2(1,0))
+                        };
 
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(1, 1));
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(0, 0));
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(1, 0));
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(1, 1));
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(0, 1));
-            //vertices[i++] = new VertexPositionTexture(center, new Vector2(0, 0));
-
-            Vector3 textPosition = new Vector3(0, 0, 10);
-            
-            basicEffect.World = Matrix.CreateConstrainedBillboard(textPosition, cameraPosition, Vector3.Up, null, null);
+            basicEffect.World = Matrix.Identity;
             basicEffect.View = viewMatrix;
             basicEffect.Projection = projectionMatrix;
+            basicEffect.TextureEnabled = true;
+            basicEffect.Texture = shipTex;
 
-            const string message = "hello, world!";
-            Vector2 textOrigin = font.MeasureString(message) / 2;
-            const float textSize = 0.75f;
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
 
-            spriteBatch.Begin(0, null, null, DepthStencilState.DepthRead, RasterizerState.CullNone, basicEffect);
-            spriteBatch.DrawString(font, message, Vector2.Zero, Color.White, 0, textOrigin, textSize, SpriteEffects.FlipHorizontally | SpriteEffects.FlipVertically, 0);
-            spriteBatch.End();
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, vertices, 0, 2);
+            }
 
             base.Draw(gameTime);
+        }
+    }
+
+    public class Quad
+    {
+        public VertexPositionNormalTexture[] Vertices;
+        public Vector3 Origin;
+        public Vector3 Up;
+        public Vector3 Normal;
+        public Vector3 Left;
+        public Vector3 UpperLeft;
+        public Vector3 UpperRight;
+        public Vector3 LowerLeft;
+        public Vector3 LowerRight;
+        public int[] Indexes;
+
+
+        public Quad(Vector3 origin, Vector3 normal, Vector3 up,
+             float width, float height)
+        {
+            this.Vertices = new VertexPositionNormalTexture[4];
+            this.Indexes = new int[6];
+            this.Origin = origin;
+            this.Normal = normal;
+            this.Up = up;
+
+            // Calculate the quad corners
+            this.Left = Vector3.Cross(normal, this.Up);
+            Vector3 uppercenter = (this.Up * height / 2) + origin;
+            this.UpperLeft = uppercenter + (this.Left * width / 2);
+            this.UpperRight = uppercenter - (this.Left * width / 2);
+            this.LowerLeft = this.UpperLeft - (this.Up * height);
+            this.LowerRight = this.UpperRight - (this.Up * height);
+
+            this.FillVertices();
+        }
+
+        private void FillVertices()
+        {
+            Vector2 textureUpperLeft = new Vector2(0.0f, 0.0f);
+            Vector2 textureUpperRight = new Vector2(1.0f, 0.0f);
+            Vector2 textureLowerLeft = new Vector2(0.0f, 1.0f);
+            Vector2 textureLowerRight = new Vector2(1.0f, 1.0f);
+
+            for (int i = 0; i < this.Vertices.Length; i++)
+            {
+                this.Vertices[i].Normal = this.Normal;
+            }
+
+            this.Vertices[0].Position = this.LowerLeft;
+            this.Vertices[0].TextureCoordinate = textureLowerLeft;
+            this.Vertices[1].Position = this.UpperLeft;
+            this.Vertices[1].TextureCoordinate = textureUpperLeft;
+            this.Vertices[2].Position = this.LowerRight;
+            this.Vertices[2].TextureCoordinate = textureLowerRight;
+            this.Vertices[3].Position = this.UpperRight;
+            this.Vertices[3].TextureCoordinate = textureUpperRight;
+
+            this.Indexes[0] = 0;
+            this.Indexes[1] = 1;
+            this.Indexes[2] = 2;
+            this.Indexes[3] = 2;
+            this.Indexes[4] = 1;
+            this.Indexes[5] = 3;
         }
     }
 }
