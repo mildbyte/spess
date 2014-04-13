6fed503ce9089f8b758b2d6983421d34723c6378
Artjoms Iskovs <ai280@cam.ac.uk>
2014-04-13 19:12:10 +0300

    Added a gate-aware route calculation

--- spess/Universe.cs
+++ spess/Universe.cs
@@ -9,10 +9,88 @@ namespace spess
         List<Sector> sectors;
         List<Owner> owners;
 
+        Dictionary<Owner, List<Gate>> ownerKnownGates;
+
         public List<Sector> Sectors { get { return sectors; } }
         public List<Owner> Owners { get { return owners; } }
 
         public Owner GetPlayer() { return owners[0]; }
+
+        class BFSNode
+        {
+            Gate prevGate;
+            BFSNode prevNode;
+            Sector sector;
+
+            public Gate PrevGate { get { return prevGate; } }
+            public BFSNode PrevNode { get { return prevNode; } }
+            public Sector Sector { get { return sector; } }
+            public BFSNode(Gate prevGate, BFSNode prevNode, Sector sector)
+            {
+                this.sector = sector;
+                this.prevGate = prevGate;
+                this.prevNode = prevNode;
+            }
+        }
+
+        /// <summary>
+        /// Gets a sequence of gates that the owner knows about that allows his ship to get from
+        /// one sector to another.
+        /// </summary>
+        /// <param name="o">Owner of the ship</param>
+        /// <param name="s1">Source sector</param>
+        /// <param name="s2">Destination sector</param>
+        /// <returns>A list of gates the ship has to take or null if there is no such route</returns>
+        public List<Gate> GetGateAwareRoute(Owner o, Sector s1, Sector s2)
+        {
+            // Drop out if we don't know the owner
+            if (!ownerKnownGates.ContainsKey(o)) return null;
+            List<Gate> knownGates = ownerKnownGates[o];
+
+            // Initialize the BFS structures
+            HashSet<Sector> visitedSectors = new HashSet<Sector>();
+            LinkedList<BFSNode> bfsQueue = new LinkedList<BFSNode>();
+            bfsQueue.AddLast(new BFSNode(null, null, s1));
+
+            // Perform a breadth-first traversal of the sector graphs, recording the gate we took
+            // and the previous BFS node
+            BFSNode currNode = null;
+            while (bfsQueue.Count > 0)
+            {
+                // Pop a node from the queue
+                currNode = bfsQueue.First.Value;
+                bfsQueue.RemoveFirst();
+                
+                // Ignore the sector if we've been there
+                if (visitedSectors.Contains(currNode.Sector)) continue;
+                visitedSectors.Add(currNode.Sector);
+
+                // Stop if we've reached the destination
+                if (currNode.Sector == s2) break;
+
+                // Take all possible gates we know about
+                foreach (Gate g in currNode.Sector.Gates)
+                {
+                    if (!knownGates.Contains(g)) continue;
+                    bfsQueue.AddLast(new BFSNode(g, currNode, g.Destination.Sector));
+                }
+            }
+
+            // If we haven't reached the sector, we've failed.
+            if (currNode.Sector != s2) return null;
+
+            // Reconstruct the list of gates we took
+            List<Gate> result = new List<Gate>();
+            while (currNode.PrevGate != null)
+            {
+                result.Add(currNode.PrevGate);
+                currNode = currNode.PrevNode;
+            }
+
+            result.Reverse();
+
+            return result;
+        }
 
         public Universe()
         {
