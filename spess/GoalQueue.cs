88120df8eafc6c0bd2b02d2e3937db38c8c54e4c
Artjoms Iskovs <ai280@cam.ac.uk>
2014-04-26 10:25:17 +0100

    Added events to the goal queue

--- spess/GoalQueue.cs
+++ spess/GoalQueue.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace spess.AI
 {
+    public delegate void OrderCompleted(IGoal goal);
+    public delegate void OrderStarted(IBaseGoal goal);
+    public delegate void OrderFailed(IGoal goal);
+
     public class GoalQueue
     {
         LinkedList<Goal> goals;
@@ -23,6 +27,9 @@ namespace spess.AI
 
         public IEnumerable<Goal> Goals { get { return goals; } }
         public IEnumerable<IBaseGoal> PendingGoals { get { return pendingGoals; } }
+        public OrderCompleted OnOrderCompleted { get; set; }
+        public OrderStarted OnOrderStarted { get; set; }
+        public OrderFailed OnOrderFailed { get; set; }
 
         public void AddGoal(Goal g)
         {
@@ -49,7 +56,11 @@ namespace spess.AI
         public void Update()
         {
             // Keep removing complete goals from the pending goal queue
-            while (pendingGoals.Any() && pendingGoals.First.Value.IsComplete()) pendingGoals.RemoveFirst();
+            while (pendingGoals.Any() && pendingGoals.First.Value.IsComplete())
+            {
+                if (OnOrderCompleted != null) OnOrderCompleted(pendingGoals.First());
+                pendingGoals.RemoveFirst();
+            }
             
             // If we still have pending goals or there are no goals that we can execute, return
             if (pendingGoals.Any() || !goals.Any()) return;
@@ -61,27 +72,26 @@ namespace spess.AI
             // If one of the cousins of this goal failed, ignore this goal.
             if (failedGoals.Contains(currGoal.Parent)) return;
 
+            // If the goal failed, add it to the failed goals' set (if it's a root goal)
+            // or add its ultimate parent to the set otherwise.
+            if (currGoal is IFailableGoal && ((IFailableGoal)currGoal).Failed())
+            {
+                if (currGoal.Parent == null) failedGoals.Add(currGoal);
+                else failedGoals.Add(currGoal.Parent);
+                if (OnOrderFailed != null) OnOrderFailed(currGoal);
+            }
+
             // For a composite goal, add all of its children to the goal queue.
             // For a base goal, execute it and move it to the pending goals' queue.
             if (currGoal is ICompositeGoal)
             {
                 var newGoals = ((ICompositeGoal)currGoal).GetSubgoals();
-
-                // If the goal failed, add it to the failed goals' set (if it's a root goal)
-                // or add its ultimate parent to the set otherwise.
-                if (currGoal is IFailableGoal && ((IFailableGoal)currGoal).Failed())
-                {
-                    if (currGoal.Parent == null) failedGoals.Add(currGoal);
-                    else failedGoals.Add(currGoal.Parent);
-                }
-                else
-                {
-                    goals = new LinkedList<Goal>(newGoals.Concat(goals));
-                }
+                goals = new LinkedList<Goal>(newGoals.Concat(goals));
             }
             else if (currGoal is IBaseGoal) {
                 ((IBaseGoal)currGoal).Execute();
                 pendingGoals.AddLast((IBaseGoal)currGoal);
+                if (OnOrderStarted != null) OnOrderStarted(currGoal as IBaseGoal);
             }
         }
     }
